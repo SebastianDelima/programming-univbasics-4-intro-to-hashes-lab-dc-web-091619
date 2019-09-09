ec68f8b0c00474dc6932a5274acb3d7af361eb78
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-09 17:47:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash.include(key) == true 
+  if hash.include?(key) == true 
    return hash[key+1]
   else 
    return hash[key => 1 ]
